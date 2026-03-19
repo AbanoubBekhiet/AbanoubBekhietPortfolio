e51e96c0c9c30d663acc6582ecd3fe036e588c47
AbanoubBekhiet <abanwbebekhiet@gmail.com>
2026-03-19 15:06:13 +0200

    optimizing project list shaking in mobile with reducing the scrub

--- app/components/ProjectsList.js
+++ app/components/ProjectsList.js
@@ -37,7 +37,7 @@ export default function ProjectsList() {
 					trigger: triggerElement,
 					pin: true,
 					start: "top top",
-					scrub: 2,
+					scrub: 0.5,
 					end: () => `+=${horizontalDistance}`,
 					invalidateOnRefresh: true,
 				},
